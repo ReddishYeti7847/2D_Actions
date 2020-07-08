0d800e775169085334233b3539870d3080c63495
ReddishYeti7847 <noriyasu200081@gmail.com>
2020-07-08 18:39:02 +0900

    左右の移動をできるようにして、ジャンプ機能の実装に取り掛かった。

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
-    public float speed = 4.0f; //移動速度
+    public float d;     //移動方向
+    public float speed; //移動速度
+    public bool isJump;    //ジャンプしたかどうか
 
     // Start is called before the first frame update
     void Start()
@@ -17,10 +19,28 @@ public class Player : MonoBehaviour
     void Update()
     {
         //キー入力(左右のみ)
-        var h = Input.GetAxis("Horizontal");
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            d = 1f;
+        } else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            d = -1f;
+        } else
+        {
+            d = 0;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            isJump = true;
+        }
 
         //移動処理(左右のみ)
-        Vector2 direction = new Vector2(h, 0).normalized;   //移動する向きの作成
-        GetComponent<Rigidbody2D>().velocity = direction * speed;   //移動する向きとスピードを代入
+        rb.velocity = new Vector2(speed * d, rb.velocity.y);
+        if (isJump)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 5f);
+            isJump = false;
+        }
     }
 }
